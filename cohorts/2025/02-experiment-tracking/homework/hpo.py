0f444205595241d9e05eaf599fdca442e84f9416
Siu Hei Wong <heodi@Sius-MacBook-Air.local>
2025-05-26 03:05:14 +0100

    Solve conflict for week 2 homework files

--- cohorts/2025/02-experiment-tracking/homework/hpo.py
+++ cohorts/2025/02-experiment-tracking/homework/hpo.py
@@ -34,13 +34,25 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        with mlflow.start_run():
+            mlflow.set_tag("model", "random_forest")
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = root_mean_squared_error(y_val, y_pred)
+            mlflow.log_metric("rmse", rmse)
 
+<<<<<<< HEAD
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
         rmse = root_mean_squared_error(y_val, y_pred)
 
         return {'loss': rmse, 'status': STATUS_OK}
+=======
+            return {'loss': rmse, 'status': STATUS_OK}
+>>>>>>> c3856e2 (Homework week 2)
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
--- cohorts/2025/02-experiment-tracking/homework/train.py
+++ cohorts/2025/02-experiment-tracking/homework/train.py
@@ -2,9 +2,14 @@ import os
 import pickle
 import click
 
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
+import mlflow
+mlflow.set_tracking_uri("sqlite:////Users/heodi/Projects/mlops-zoomcamp/02-experiment-tracking/mlflow.db")
+mlflow.set_experiment("02-experiment-tracking-homework")
+mlflow.sklearn.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -17,16 +22,25 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
+
 def run_train(data_path: str):
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
+<<<<<<< HEAD
     rf = RandomForestRegressor(max_depth=10, random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
 
     rmse = root_mean_squared_error(y_val, y_pred)
+=======
+        rmse = root_mean_squared_error(y_val, y_pred)
+>>>>>>> c3856e2 (Homework week 2)
 
 
 if __name__ == '__main__':
